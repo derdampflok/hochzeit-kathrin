5f2c1d094000d4dd58826985fcfd2920c92a1cf5
derdampflok <kroft.domi@t-online.de>
2026-03-25 17:20:42 +0100

    add all layout components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import './App.scss'
 import DecorativeBackground from './components/DecorativeBackground'
 import HeroSection from './components/HeroSection'
 import RsvpSection from './components/RsvpSection'
+import Location from './components/Location'
+import Schedule from './components/Schedule'
+import HotelsParking from './components/HotelsParking'
+import Contact from './components/Contact'
 
 function App() {
   return (
@@ -9,6 +13,10 @@ function App() {
       <DecorativeBackground />
       <HeroSection />
       <RsvpSection />
+      <Location />
+      <Schedule />
+      <HotelsParking />
+      <Contact />
     </main>
   )
 }
---
+++ src/components/HotelsParking.tsx
@@ -0,0 +1,5 @@
+function HotelsParking() {
+    return <h1>HotelsParking</h1>
+}
+
+export default HotelsParking
