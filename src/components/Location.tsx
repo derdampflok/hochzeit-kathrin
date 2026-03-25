cb2513b513a27866a838c303ae3484bd56add580
derdampflok <kroft.domi@t-online.de>
2026-03-25 18:36:53 +0100

    add google maps

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,13 +1,27 @@
 import Card from "./Card"
 import CardContent from "./CardContent"
 import CardHeader from "./CardHeader"
+import "./Location.scss"
 
 function Location() {
     return (
         <Card>
             <CardHeader title="Location"></CardHeader>
-            <CardContent>
-                <h1>Location</h1>
+            <CardContent className="location-maps">
+                <iframe
+                    className="location-map"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2592.6676218273597!2d10.993122876389025!3d49.4718946573372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479f55fbd2d9e605%3A0xfc3605eb38bbbcc2!2sAuferstehungskirche!5e0!3m2!1sde!2sde!4v1774459785633!5m2!1sde!2sde"
+                    allowFullScreen
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade">
+                </iframe>
+                <iframe
+                    className="location-map"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5171.7983516775475!2d11.002909876395984!3d49.599650748257325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a1f8e72bf78a3f%3A0xd3b9eeffcb6ac807!2sRedoutensaal%20-%20Erlangen!5e0!3m2!1sde!2sde!4v1774459944431!5m2!1sde!2sde"
+                    allowFullScreen
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade">
+                </iframe>
             </CardContent>
         </Card>
     )
